c3c26b3354b49f0cb0eb0b0950b38e08eeb9d6d7
febinpsunny <pasfebin@gmail.com>
2023-06-07 18:58:58 -0600

    TED learning rate uodate and error calc modifications

--- TED_Verification.py
+++ TED_Verification.py
@@ -6,13 +6,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.stats import mode
 import sys
-from pprint import pprint
 import math
-from pandas import *
 
 
 def phaseChange(size:int=10):
-    # Generate phae matrices and calculate difference to kick off the evaluation
+    # Generate phae matrices and calculate difference to kick off the algorithm
     # Considering phase values to be in radians ranging from 0 to 2π
     stopLim=int(round(2*np.pi,5)*pow(10,5))
 
@@ -34,7 +32,6 @@ def radianManagement(inputPhaseMat:np.array):
     twoπ=int(round(2*np.pi,5)*pow(10,5))
 
     for i in inputPhaseMat:
-        #print(i)
         if i>0:
             phaseMat=np.append(phaseMat, (i*pow(10,5)%twoπ)/pow(10,5))
         elif i<0:
@@ -73,7 +70,7 @@ def TMatrixGen_rand(size:int=10):
     return T
 #* Thermal crosstalk coefficient calculation utilities: END 
 
-#* Error calculation utilities: BEGIN 
+#* Loss function calculation utilities: BEGIN 
 def meanErrorCalc(Φ:np.array):
     error =0
     for i in Φ:
@@ -90,53 +87,57 @@ def modeErrorCalc(Φ:np.array):
     return mode(Φ)[0]
 
 def maxErrorCalc(Φ:np.array):
-    return np.max(Φ)
+    # This needs careful consideration as we need maximum absolute value, but should preserve 
+    # the sign of the value; i.e. np.max() alone is not useful and will lead to a constant 
+    # +ve step direction leading to the algorithm never converging
+    max=0
+    min=0
+    for i in Φ:
+        if i> max:
+            max =i
+        if i<min:
+            min = i
+    if np.abs(max)>np.abs(min):
+        return max
+    else:
+        return min
 
-#* Error calculation utilities: END 
+# Modified MSE which preserves the directionality of the error
+def meanSquareErrorMod(Φ:np.array):
+    squaresum=0
+    for i in Φ:
+        squaresum= squaresum+(i*i)
+    MSE = squaresum/len(Φ)
+
+    return MSE*Φ[-1]/np.abs(Φ[-1])
+
+#* Loss function calculation utilities: END
+
+def matrixPrint(matrix:np.array):
+    for i in matrix:
+        print(*i)
+    return None
 
 #* Core algorithm functions: BEGIN 
 def Ted_Algorithm(Φ:np.array , δΦ:np.array, T:np.array):
     # Iteratively calculate error function, update phase variables, relaunch TED iteration
     errorVal=[]
+    
+    # Velocity term for momentum calculation
+    velocity =[0]
 
     Φ_old = Φ
     δΦ_old = δΦ
 
-    stepDir='+'
+    #* The following term 'Λ_rate' is a learning rate; but becasue of the values involved in this solution space it is set at 1, initially
+    Λ_rate = 1
 
     Λ, P = np.linalg.eig(T)
     P_inv = np.linalg.inv(P)
     T_D = np.diag(Λ)
+    # TODO: Possible principle component analysis (PCA) implementation for larger MR array sizes
 
-    while(True):
-        error=0
-        Φ_new = TED_iteration(Φ_old , δΦ_old, stepDir, P, P_inv, T_D, T)
-
-        δΦ_new = Φ_new-Φ_old
-        #print(δΦ_new)
-        error = meanErrorCalc(δΦ_new)
-
-        Φ_old = Φ_new
-        δΦ_old = δΦ_new
-
-        errorVal = np.append(errorVal, error)
-        if math.ceil(math.log(np.abs(error),10))<=(-3):
-            return errorVal, δΦ_old
-        else:
-            if error>0:
-                stepDir='-'
-            elif error<0:
-                stepDir='+'
-            else:
-                return errorVal, δΦ_old
-
-    return errorVal, δΦ_old
-
-def TED_iteration(Φ:np.array , δΦ:np.array, stepDir:str, P:np.array, P_inv:np.array, T_D:np.array, T:np.array):
-
-    δΦ_cap = np.array(np.real(np.dot(T,δΦ)))
-
-    # Sanity check 1
+    #* Sanity check 1
     # For ensuring that the method used to calculate the three variables above make sense 
     # This is direct eigen decomposition [special case of singular value decomposition (SVD) for square matrices]
     # Utilizes eq(2) from Section II of the TED paper
@@ -146,11 +147,42 @@ def TED_iteration(Φ:np.array , δΦ:np.array, stepDir:str, P:np.array, P_inv:np
         print(*l)
     '''
     # The error analysis should show that the error between T and T_new are negligible (~16 orders of magnitude below 0)
+    
+    while(True):
+        error=0
+        
+        Φ_new = TED_iteration(Φ_old , δΦ_old, Λ_rate, P, P_inv, T_D, T)
+
+        δΦ_new = Φ_new-Φ_old
+        #* This error term is basically the loss function of this algorithm and 
+        #* we shall be trying to minimize it as much as possible, as fast as possible
+        error = meanErrorCalc(δΦ_new)
+
+        Φ_old = Φ_new
+        δΦ_old = δΦ_new
+
+        errorVal = np.append(errorVal, error)
+
+        if math.floor(math.log(np.abs(error),10))<=(-3):
+            #* Threshold based stopping for the algorithm iteration
+            return errorVal, δΦ_old
+        else:
+            #* Learning rate updation based on observed
+            #Λ_rate = learningRateMomentumUpdate(errorVal, velocity, Λ_rate)
+            Λ_rate=learningRateUpdate(errorVal, Λ_rate)
+            if Λ_rate==0:
+                return errorVal, δΦ_old
+            else:
+                continue          
+
+def TED_iteration(Φ:np.array , δΦ:np.array, Λ_rate:float, P:np.array, P_inv:np.array, T_D:np.array, T:np.array):
+
+    δΦ_cap = np.array(np.real(np.dot(T,δΦ)))
 
     δΨ = np.array(np.real(np.dot(P_inv, δΦ)))
     δΨ_cap = np.array(np.real(np.dot(P_inv, δΦ_cap)))
     
-    # Sanity check 2 
+    #* Sanity check 2 
     # To ensure δΨ_cap calculation is on track
     # From Section II, eq(2) and eq(3) of the TED paper:
     # δΨ_cap=T_D.δΨ
@@ -164,22 +196,59 @@ def TED_iteration(Φ:np.array , δΦ:np.array, stepDir:str, P:np.array, P_inv:np
 
     Ψ = np.array(np.real(np.dot(P_inv, Φ)))
 
-    # Sanity check 3
+    #* Sanity check 3
     # Ensure Φ can be regenerated from the calculated Ψ matrix
     '''
     print(Φ - np.real(np.dot(P,Ψ)))
     '''
     # This check should also generate negligible errors
-    if stepDir=='+':
+    
+    print(Λ_rate)
+    if Λ_rate >0:
         Ψ_new = Ψ + δΨ
-    elif stepDir=='-':
+    else:
         Ψ_new = Ψ - δΨ
-    #print("New Ψ matrix:",Ψ_new)
+    '''
+    print(Λ_rate)
+    Ψ_new = Ψ - Λ_rate*δΨ
+    '''
+
     Φ_new = np.array(np.real(np.dot(P,Ψ_new)))
     
     Φ_new = radianManagement(Φ_new)
     
     return Φ_new
+
+def learningRateUpdate(errorVal:np.array, Λ_rate:float):
+    if errorVal[-1]>0:
+        Λ_rate = np.abs(Λ_rate)*-1
+    elif errorVal[-1]<0:
+        Λ_rate = np.abs(Λ_rate)
+    else:
+        Λ_rate=0
+
+    return Λ_rate
+
+# Since the Φ matrix updation process is similar to SGD, we opt for a momentum based learning rate updation
+def learningRateMomentumUpdate(errorVal:np.array, velocity:np.array, Λ_rate:float):
+    # Defining momentum coefficient
+    β=0.75
+    # Momentum term calculation
+    v = β*velocity[-1] + (1-β)*errorVal[-1]
+    # Learning rate modification
+    Λ_rate = Λ_rate*v
+    # Updating velocity list
+    velocity.append(v)
+    # Setting step direction
+    if errorVal[-1]>0:
+        Λ_rate = np.abs(Λ_rate)*-1
+    elif errorVal[-1]<0:
+        Λ_rate = np.abs(Λ_rate)
+    else:
+        Λ_rate=0
+
+    return Λ_rate
+
 #* Core algorithm functions: END
 
 #* Graphing utility
